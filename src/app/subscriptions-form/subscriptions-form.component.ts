12accea421d32f2035a5d0311bdbaeca5df89917
wilsonkinyua <wilsonkinyuam@gmail.com>
2021-08-19 23:17:49 +0300

    form validation done

--- src/app/subscriptions-form/subscriptions-form.component.ts
+++ src/app/subscriptions-form/subscriptions-form.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-subscriptions-form',
   templateUrl: './subscriptions-form.component.html',
-  styleUrls: ['./subscriptions-form.component.css']
+  styleUrls: ['./subscriptions-form.component.css'],
 })
 export class SubscriptionsFormComponent implements OnInit {
+  @ViewChild('f') subscribeForm: NgForm;
 
-  constructor() { }
+  selectedSubscription = 'Advanced';
+  subscriptionFormSubmitted = false;
 
-  ngOnInit(): void {
+  subscriber = {
+    email: '',
+    subscription: '',
+    password: '',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  // functions
+  onSubmit() {
+    this.subscriptionFormSubmitted = true;
+    this.subscriber.email = this.subscribeForm.value.email;
+    this.subscriber.subscription = this.subscribeForm.value.subscription;
+    this.subscriber.password = this.subscribeForm.value.password;
+    // reset form
+    this.subscribeForm.reset();
   }
-
 }
